0448f74ae29ea2b8a9d2b69cb3a3b908bdd507ec
Sandeep Mistry <sandeep.mistry@gmail.com>
2013-11-05 11:31:22 -0500

    - support for advertising iBeacon on OS X

---
+++ test-ibeacon.js
@@ -0,0 +1,23 @@
+var util = require('util');
+
+var bleno = require('./index');
+
+console.log('bleno');
+
+bleno.on('stateChange', function(state) {
+  console.log('on -> stateChange: ' + state);
+
+  if (state === 'poweredOn') {
+    bleno.startAdvertisingIBeacon('e2c56db5dffb48d2b060d0f5a71096e0', 0, 0, -59);
+  } else {
+    bleno.stopAdvertising();
+  }
+});
+
+bleno.on('advertisingStart', function() {
+  console.log('on -> advertisingStart');
+});
+
+bleno.on('advertisingStop', function() {
+  console.log('on -> advertisingStop');
+});
