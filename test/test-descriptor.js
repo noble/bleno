b62fe0ce15ff77eae9cd406f29232e9256325f2c
Sandeep Mistry <sandeep.mistry@gmail.com>
2013-11-07 16:16:10 -0500

    - basic tests for characteristic, descriptor, primary service

---
+++ test/test-descriptor.js
@@ -0,0 +1,47 @@
+var should = require('should');
+
+var Descriptor = require('../lib/descriptor');
+
+describe('Descriptor', function() {
+  var mockUuid = 'mock-uuid';
+  var mockValue = new Buffer('mock value');
+
+  it('should create with uuid option', function() {
+    var descriptor = new Descriptor({
+      uuid: mockUuid
+    });
+
+    descriptor.uuid.should.equal(mockUuid);
+
+    Buffer.isBuffer(descriptor.value).should.equal(true);
+    descriptor.value.length.should.equal(0);
+  });
+
+  it('should create with value option', function() {
+    var descriptor = new Descriptor({
+      value: mockValue
+    });
+
+    descriptor.value.should.equal(mockValue);
+  });
+
+  describe('toString', function() {
+    it('should hex buffer value', function() {
+      var descriptor = new Descriptor({
+        uuid: mockUuid,
+        value: mockValue
+      });
+
+      descriptor.toString().should.equal('{"uuid":"mock-uuid","value":"6d6f636b2076616c7565"}');
+    });
+
+    it('should leave non-buffer value alone', function() {
+      var descriptor = new Descriptor({
+        uuid: mockUuid,
+        value: 'mock value'
+      });
+
+      descriptor.toString().should.equal('{"uuid":"mock-uuid","value":"mock value"}');
+    });
+  });
+});
---
+++ test/test-primary-service.js
@@ -0,0 +1,35 @@
+var should = require('should');
+
+var PrimaryService = require('../lib/primary-service');
+
+describe('PrimaryService', function() {
+  var mockUuid = 'mock-uuid';
+  var mockCharacteristics = [{}, {}, {}];
+
+  it('should create with uuid option', function() {
+    var service = new PrimaryService({
+      uuid: mockUuid
+    });
+
+    service.uuid.should.equal(mockUuid);
+
+    Array.isArray(service.characteristics).should.equal(true);
+    service.characteristics.length.should.equal(0);
+  });
+
+  it('should create with characteristics option', function() {
+    var service = new PrimaryService({
+      characteristics: mockCharacteristics
+    });
+
+    service.characteristics.should.equal(mockCharacteristics);
+  });
+
+  it('should toString', function() {
+    var service = new PrimaryService({
+      uuid: mockUuid
+    });
+
+    service.toString().should.equal('{"uuid":"mock-uuid","characteristics":[]}');
+  });
+});
