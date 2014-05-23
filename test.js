2895424c107655670c4508b532df7488f76add34
Jonathan Wiesel <jonathanwiesel@gmail.com>
2014-05-23 08:56:09 -0430

    Add on.disconnect clientAddress parameter emiting

--- test.js
+++ test.js
@@ -100,7 +100,7 @@ function SampleService() {
   SampleService.super_.call(this, {
     uuid: 'fffffffffffffffffffffffffffffff0',
     characteristics: [
-      new StaticReadOnlyCharacteristic(),   
+      new StaticReadOnlyCharacteristic(),
       new DynamicReadOnlyCharacteristic(),
       new WriteOnlyCharacteristic(),
       new NotifyOnlyCharacteristic()
@@ -123,12 +123,12 @@ bleno.on('stateChange', function(state) {
 // Linux only events /////////////////
 bleno.on('accept', function(clientAddress) {
   console.log('on -> accept, client: ' + clientAddress);
-  
+
   bleno.updateRssi();
 });
 
-bleno.on('disconnect', function() {
-  console.log('on -> disconnect');
+bleno.on('disconnect', function(clientAddress) {
+  console.log('on -> disconnect, client: ' + clientAddress);
 });
 
 bleno.on('rssiUpdate', function(rssi) {
