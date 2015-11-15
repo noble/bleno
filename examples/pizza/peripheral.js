6e7c18df5062e85635f848196d6cd6a6c1a75093
Sandeep Mistry <sandeep.mistry@gmail.com>
2015-11-15 11:21:15 -0500

    Revert flipping services in pizza example

--- examples/pizza/peripheral.js
+++ examples/pizza/peripheral.js
@@ -24,7 +24,6 @@ var PizzaService = require('./pizza-service');
 //
 var name = 'PizzaSquat';
 var pizzaService = new PizzaService(new pizza.Pizza());
-var pizzaService2 = new PizzaService(new pizza.Pizza());
 
 //
 // Wait until the BLE radio powers on before attempting to advertise.
@@ -54,16 +53,8 @@ bleno.on('advertisingStart', function(err) {
     // Once we are advertising, it's time to set up our services,
     // along with our characteristics.
     //
-    bleno.setServices([ pizzaService ], function (){
-        var flip = false;
-        setInterval(function (){
-            if(flip){
-              bleno.removeService(pizzaService2);
-            } else {
-              bleno.addService(pizzaService2);
-            }
-            flip = !flip;
-        }, 1500);
-    });
+    bleno.setServices([
+      pizzaService
+    ]);
   }
 });
